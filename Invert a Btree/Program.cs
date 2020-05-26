4c9058ed181a3e7b235063c0d7b9ea585bb65e4e
Chinmay <Chinmay8116@gmail.com>
2020-05-26 10:58:51 +0530

    commit-unilvalued and invert

---
+++ Invert a Btree/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace Invert_a_Btree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            TreeNode p = root;
+            queue.Enqueue(p);
+            while (queue.Count != 0)
+            {
+                TreeNode current = queue.Dequeue();
+                var temp = current.left;
+                current.left = current.right;
+                current.right = temp;
+                if (current.left != null) queue.Enqueue(current.left);
+                if (current.right != null) queue.Enqueue(current.right);
+            }
+            return p;
+        }
+        public TreeNode RInvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            TreeNode right = RInvertTree(root.right);
+            TreeNode left = RInvertTree(root.left);
+            root.left = right;
+            root.right = left;
+            return root;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Univalued BTree/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://leetcode.com/problems/univalued-binary-tree/
+namespace Univalued_BTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static class AppHelper
+    {
+        public static Boolean IsUnivalTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            List<Int32> lst = new List<Int32>();
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            TreeNode p = root;
+            lst.Add(p.val);
+            q.Enqueue(p);
+            while (q.Count != 0)
+            {
+                p = q.Dequeue();
+                lst.Add(p.val);
+                if (p.left != null)
+                {
+                    q.Enqueue(p.left);
+                }
+                if (p.right != null)
+                {
+                    q.Enqueue(p.right);
+                }
+            }
+            return lst.All(g => g == lst.First());
+        }
+    }
+    public class Solution
+    {
+        public bool IsUnivalTree(TreeNode root)
+        {
+            return AppHelper.IsUnivalTree(root);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
